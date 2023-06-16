c7f646804adaf6b4da44d80ee8b15306b2dc54ed
rendy eka <rendimana1997@gmail.com>
2023-06-16 18:47:20 +0700

    conditional-list

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -12,14 +12,29 @@ function Expenses(props){
         setFilteredYear(selectedYear)
     }
 
-    // expense item, expenses filter --> coming soon
+    // expense filter
+    const filteredExpense = props.items.filter(expense => {
+        return expense.date.getFullYear().toString() === filteredYear;
+    })
+
+
+    // expense default value if items == 0
+    let expenseContent = <p className="expenses-list__fallback">Not Found</p>;
+
+    if(filteredExpense.length > 0){
+        expenseContent = filteredExpense.map(expense => (
+            <ExpenseItem key={expense.id} title={expense.title} amount={expense.amount} date={expense.date}>
+            </ExpenseItem>
+        ))
+    }
+
+
+    // expense item
     return(
         <div>
             <Card className="expenses">
                 <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-                <ExpenseItem title={props.items[0].title} amount={props.items[0].amount} date={props.items[0].date}/>
-                <ExpenseItem title={props.items[1].title} amount={props.items[1].amount} date={props.items[1].date}/>
-                <ExpenseItem title={props.items[2].title} amount={props.items[2].amount} date={props.items[2].date}/>
+                {expenseContent}
             </Card>
         </div>
     )
